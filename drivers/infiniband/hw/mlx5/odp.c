ea3bcfa2a656b1977fe041533fd89980cd28612c
Leon Romanovsky <leonro@nvidia.com>
2023-10-31 12:57:56 +0200

    RDMA/core: Separate DMA mapping to caching IOVA and page linkage
    Reuse newly added DMA API to cache IOVA and only link/unlink pages
    in fast path.
    
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>

--- drivers/infiniband/hw/mlx5/odp.c
+++ drivers/infiniband/hw/mlx5/odp.c
@@ -164,6 +164,7 @@ static void populate_mtt(__be64 *pas, size_t idx, size_t nentries,
 {
 	struct ib_umem_odp *odp = to_ib_umem_odp(mr->umem);
 	bool downgrade = flags & MLX5_IB_UPD_XLT_DOWNGRADE;
+	struct ib_device *dev = odp->umem.ibdev;
 	unsigned long pfn;
 	dma_addr_t pa;
 	size_t i;
@@ -177,12 +178,17 @@ static void populate_mtt(__be64 *pas, size_t idx, size_t nentries,
 			/* Initial ODP init */
 			continue;
 
-		pa = odp->dma_list[idx + i];
+		pa = dma_hmm_link_page(&odp->state, &odp->pfn_list[idx + i],
+				       (idx + i) * (1 << odp->page_shift));
+		WARN_ON_ONCE(ib_dma_mapping_error(dev, pa));
+		odp->pfn_list[idx + i] |= HMM_PFN_DMA_MAPPED;
+
 		pa |= MLX5_IB_MTT_READ;
 		if ((pfn & HMM_PFN_WRITE) && !downgrade)
 			pa |= MLX5_IB_MTT_WRITE;
 
 		pas[i] = cpu_to_be64(pa);
+		odp->npages++;
 	}
 }
 
