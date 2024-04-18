000051259e551e839d1b0bddb50e72e56e30c326
Leon Romanovsky <leonro@nvidia.com>
2024-04-18 20:42:42 +0300

    dma-mapping: implement link range API
    Introduce new DMA APIs to perform DMA linkage of buffers
    in layers higher than DMA.
    
    In proposed API, the callers will perform the following steps.
    In map path:
    	if (dma_can_use_iova(...))
    	    dma_alloc_iova()
    	    for (page in range)
    	       dma_link_range(...)
    	else
    	     /* Fallback to legacy map pages */
                 for (all pages)
    	       dma_map_page(...)
    In unmap path:
    	if (dma_can_use_iova(...))
    	     dma_destroy_iova()
    	else
    	     for (all pages)
    		dma_unmap_page(...)
    
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>

--- kernel/dma/mapping.c
+++ kernel/dma/mapping.c
@@ -1032,3 +1032,44 @@ void dma_free_iova(struct dma_iova_state *state)
 	iommu_dma_free_iova(state);
 }
 EXPORT_SYMBOL_GPL(dma_free_iova);
+
+/**
+ * dma_destroy_iova - Destroy IOVA range
+ * @state: IOVA state
+ * @dma_addr: First linked IOVA address
+ * @size: Size to unlink
+ *
+ * Unlink whole IOVA range and free an IOVA space
+ */
+void dma_destroy_iova(struct dma_iova_state *state, dma_addr_t dma_addr,
+		      size_t size)
+{
+	if (!use_dma_iommu(state->dev))
+		return;
+
+	iommu_dma_destroy_iova(state, dma_addr, size);
+}
+EXPORT_SYMBOL_GPL(dma_destroy_iova);
+
+/**
+ * dma_link_range_attrs - Link a range of IOVA space
+ * @state: IOVA state
+ * @phys: physical address to link
+ * @size: size of the buffer
+ * @attrs: attributes of mapping properties
+ *
+ * Link a range of IOVA space for the given IOVA state.
+ *
+ * Returns -ERMOTEIO if the range requires bounce buffering or points to
+ * P2P memory.  In this case the callers needs to call dma_map_page()
+ * directly for the range.
+ */
+int dma_link_range_attrs(struct dma_iova_state *state, phys_addr_t phys,
+			 size_t size, unsigned long attrs)
+{
+	if (!dma_can_use_iova(state))
+		return 0;
+
+	return iommu_dma_link_range(state, phys, state->range_size, size, attrs);
+}
+EXPORT_SYMBOL_GPL(dma_link_range_attrs);
