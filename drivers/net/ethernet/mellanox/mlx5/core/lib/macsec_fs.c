fcdbd785cbf63def5be918f8d785110f37389066
Leon Romanovsky <leonro@nvidia.com>
2025-05-15 11:47:22 -0400

    net/mlx5e: Use same types for macsec SCI comparison
    Commit citied in Fixes line still produces the sparse error:
    drivers/net/ethernet/mellanox/mlx5/core/lib/macsec_fs.c:1803:14:
        warning: restricted __be64 degrades to integer
    
    It is happening because there is a mismatch between different types,
    where comparison is performed with be64 vs. u32. As a solution, let's
    make sure that all operations are performed on u32 types.
    
    Fixes: d550956458a8 ("net/mlx5e: Fix wrong bitwise comparison usage in macsec_fs_rx_add_rule function")
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>

--- drivers/net/ethernet/mellanox/mlx5/core/lib/macsec_fs.c
+++ drivers/net/ethernet/mellanox/mlx5/core/lib/macsec_fs.c
@@ -1800,7 +1800,7 @@ macsec_fs_rx_add_rule(struct mlx5_macsec_fs *macsec_fs,
 	rx_rule->rule[0] = rule;
 
 	/* Rx crypto table without SCI rule */
-	if ((cpu_to_be64((__force u64)attrs->sci) & 0xFFFF) == ntohs(MACSEC_PORT_ES)) {
+	if (lower_16_bits((__force u64)attrs->sci) == ntohs(MACSEC_PORT_ES)) {
 		memset(spec, 0, sizeof(struct mlx5_flow_spec));
 		memset(&dest, 0, sizeof(struct mlx5_flow_destination));
 		memset(&flow_act, 0, sizeof(flow_act));
