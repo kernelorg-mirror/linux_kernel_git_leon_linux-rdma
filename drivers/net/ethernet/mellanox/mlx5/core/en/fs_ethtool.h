0c3e1863ebe37e103a081252c2bae4352160a61e
Leon Romanovsky <leonro@nvidia.com>
2025-05-29 08:13:34 -0400

    net/mlx5e: Refresh copyright affiliation, year and use SPDX tags
    Update all files which are touched in this series to use SPDX tags
    instead of license text and refresh the copyright year together with
    affiliation.
    
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en/fs_ethtool.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en/fs_ethtool.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0 OR Linux-OpenIB */
-/* Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. */
+/* Copyright (c) 2022-2925, NVIDIA CORPORATION & AFFILIATES. */
 
 #ifndef __MLX5E_FS_ETHTOOL_H__
 #define __MLX5E_FS_ETHTOOL_H__
