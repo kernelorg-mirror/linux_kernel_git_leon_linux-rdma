7130db958ea4d4a709781471de732ff62c2a40c1
Leon Romanovsky <leonro@nvidia.com>
2025-05-29 08:26:40 -0400

    net/mlx5e: Delete leftovers from FPGA IPsec metadata removal
    FPGA IPsec metadata handling logic was removed in commit df439fcb1cd4
    ("net/mlx5: Delete metadata handling logic") and these MLX5E_METADATA_ETHER_*
    are not needed anymore and can be deleted.
    
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en.h
@@ -39,9 +39,6 @@
 extern const struct net_device_ops mlx5e_netdev_ops;
 struct page_pool;
 
-#define MLX5E_METADATA_ETHER_TYPE (0x8CE4)
-#define MLX5E_METADATA_ETHER_LEN 8
-
 #define MLX5E_ETH_HARD_MTU (ETH_HLEN + VLAN_HLEN + ETH_FCS_LEN)
 
 #define MLX5E_HW2SW_MTU(params, hwmtu) ((hwmtu) - ((params)->hard_mtu))
