127888063ffec54f35ad4950e4fc18c488c198f7
Leon Romanovsky <leonro@nvidia.com>
2025-06-11 09:07:09 -0400

    mm/hmm: Avoid struct page use completely in DMA flow
    Convert HMM DMA API to rely on physical address even in non-IOVA
    case.
    
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>

--- mm/hmm.c
+++ mm/hmm.c
@@ -748,9 +748,10 @@ dma_addr_t hmm_dma_map_pfn(struct device *dev, struct hmm_dma_map *map,
 
 	switch (pci_p2pdma_state(p2pdma_state, dev, page)) {
 	case PCI_P2PDMA_MAP_NONE:
+		attrs |= DMA_ATTR_CPU_HOST;
 		break;
 	case PCI_P2PDMA_MAP_THRU_HOST_BRIDGE:
-		attrs |= DMA_ATTR_SKIP_CPU_SYNC;
+		attrs |= DMA_ATTR_SKIP_CPU_SYNC | DMA_ATTR_MMIO;
 		pfns[idx] |= HMM_PFN_P2PDMA;
 		break;
 	case PCI_P2PDMA_MAP_BUS_ADDR:
@@ -779,8 +780,8 @@ dma_addr_t hmm_dma_map_pfn(struct device *dev, struct hmm_dma_map *map,
 		if (WARN_ON_ONCE(dma_need_unmap(dev) && !dma_addrs))
 			goto error;
 
-		dma_addr = dma_map_page(dev, page, 0, map->dma_entry_size,
-					DMA_BIDIRECTIONAL);
+		dma_addr = dma_map_phys(dev, paddr, map->dma_entry_size,
+					DMA_BIDIRECTIONAL, attrs);
 		if (dma_mapping_error(dev, dma_addr))
 			goto error;
 
@@ -816,16 +817,23 @@ bool hmm_dma_unmap_pfn(struct device *dev, struct hmm_dma_map *map, size_t idx)
 		return false;
 
 	if (pfns[idx] & HMM_PFN_P2PDMA_BUS)
-		; /* no need to unmap bus address P2P mappings */
-	else if (dma_use_iova(state)) {
+		goto out; /* no need to unmap bus address P2P mappings */
+
+	if (pfns[idx] & HMM_PFN_P2PDMA)
+		attrs |= DMA_ATTR_MMIO;
+	else
+		attrs |= DMA_ATTR_CPU_HOST;
+
+	if (dma_use_iova(state)) {
 		if (pfns[idx] & HMM_PFN_P2PDMA)
 			attrs |= DMA_ATTR_SKIP_CPU_SYNC;
 		dma_iova_unlink(dev, state, idx * map->dma_entry_size,
 				map->dma_entry_size, DMA_BIDIRECTIONAL, attrs);
 	} else if (dma_need_unmap(dev))
-		dma_unmap_page(dev, dma_addrs[idx], map->dma_entry_size,
-			       DMA_BIDIRECTIONAL);
+		dma_unmap_phys(dev, dma_addrs[idx], map->dma_entry_size,
+			       DMA_BIDIRECTIONAL, attrs);
 
+out:
 	pfns[idx] &=
 		~(HMM_PFN_DMA_MAPPED | HMM_PFN_P2PDMA | HMM_PFN_P2PDMA_BUS);
 	return true;
