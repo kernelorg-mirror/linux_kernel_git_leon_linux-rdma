a58b01dcd6c576e1e2ac87ed7aec65fc02ef1c9d
Leon Romanovsky <leonro@nvidia.com>
2025-10-27 14:35:56 -0400

    nvme-pci: Use size_t for length fields to handle larger sizes
    This patch changes the length variables from unsigned int to size_t.
    Using size_t ensures that we can handle larger sizes, as size_t is
    always equal to or larger than the previously used u32 type.
    
    Originally, u32 was used because blk-mq-dma code evolved from
    scatter-gather implementation, which uses unsigned int to describe length.
    This change will also allow us to reuse the existing struct phys_vec in places
    that don't need scatter-gather.
    
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>

--- block/blk-mq-dma.c
+++ block/blk-mq-dma.c
@@ -8,7 +8,7 @@
 
 struct phys_vec {
 	phys_addr_t	paddr;
-	u32		len;
+	size_t		len;
 };
 
 static bool __blk_map_iter_next(struct blk_map_iter *iter)
@@ -108,7 +108,7 @@ static bool blk_rq_dma_map_iova(struct request *req, struct device *dma_dev,
 		struct phys_vec *vec)
 {
 	enum dma_data_direction dir = rq_dma_dir(req);
-	unsigned int mapped = 0;
+	size_t mapped = 0;
 	int error;
 
 	iter->addr = state->addr;
@@ -291,8 +291,10 @@ int __blk_rq_map_sg(struct request *rq, struct scatterlist *sglist,
 	blk_rq_map_iter_init(rq, &iter);
 	while (blk_map_iter_next(rq, &iter, &vec)) {
 		*last_sg = blk_next_sg(last_sg, sglist);
-		sg_set_page(*last_sg, phys_to_page(vec.paddr), vec.len,
-				offset_in_page(vec.paddr));
+
+		WARN_ON_ONCE(overflows_type(vec.len, unsigned int));
+		sg_set_page(*last_sg, phys_to_page(vec.paddr),
+			    (unsigned int)vec.len, offset_in_page(vec.paddr));
 		nsegs++;
 	}
 
@@ -411,7 +413,9 @@ int blk_rq_map_integrity_sg(struct request *rq, struct scatterlist *sglist)
 
 	while (blk_map_iter_next(rq, &iter, &vec)) {
 		sg = blk_next_sg(&sg, sglist);
-		sg_set_page(sg, phys_to_page(vec.paddr), vec.len,
+
+		WARN_ON_ONCE(overflows_type(vec.len, unsigned int));
+		sg_set_page(sg, phys_to_page(vec.paddr), (unsigned int)vec.len,
 				offset_in_page(vec.paddr));
 		segments++;
 	}
