e97a69103964bc1fb8f5f64ce59a70eaded39bfc
Leon Romanovsky <leonro@nvidia.com>
2026-02-10 09:05:30 -0500

    RDMA/bnxt_re: Reduce CQ memory footprint
    There is no need to store resize_cqe and resize_umem in CQ object.
    Let's remove them.
    
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>

--- drivers/infiniband/hw/bnxt_re/ib_verbs.h
+++ drivers/infiniband/hw/bnxt_re/ib_verbs.h
@@ -108,8 +108,6 @@ struct bnxt_re_cq {
 	struct bnxt_qplib_cqe	*cql;
 #define MAX_CQL_PER_POLL	1024
 	u32			max_cql;
-	struct ib_umem		*resize_umem;
-	int			resize_cqe;
 	void			*uctx_cq_page;
 	struct hlist_node	hash_entry;
 };
