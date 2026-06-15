b3d853098004c1c52ef3afa12d12c4c45e4c4192
Leon Romanovsky <leonro@nvidia.com>
2026-06-15 07:59:31 -0400

    PCI/P2PDMA: Add Nvidia Vera Rubin device to P2P whitelist
    Nvidia Vera Rubin platforms support PCI peer‑to‑peer transactions.
    Add them to the P2P whitelist to enable this functionality.
    
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>

--- drivers/pci/p2pdma.c
+++ drivers/pci/p2pdma.c
@@ -550,6 +550,11 @@ static const struct pci_p2pdma_whitelist_entry {
 	{PCI_VENDOR_ID_INTEL,	0x09a2, 0},
 	/* Google SoCs. */
 	{PCI_VENDOR_ID_GOOGLE,	PCI_ANY_ID, 0},
+	/* Nvidia CPUs */
+	{PCI_VENDOR_ID_NVIDIA, 0x2f95, 0},
+	{PCI_VENDOR_ID_NVIDIA, 0x2f96, 0},
+	{PCI_VENDOR_ID_NVIDIA, 0x2f97, 0},
+	{PCI_VENDOR_ID_NVIDIA, 0x2f98, 0},
 	{}
 };
 
